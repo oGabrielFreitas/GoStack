099130a5cfe195532e7230558b10493f5615e4f8
Gabriel Freitas <ogabrielfreitas@gmail.com>
2023-09-02 00:29:40 -0300

    Initial Boilerplate

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+
+app.get('/', (request, response) => response.json({message: 'Hello World'}));
+
+
+app.listen(3333, () => {
+	console.log('🚀 Server stated on port 3333!');
+});
