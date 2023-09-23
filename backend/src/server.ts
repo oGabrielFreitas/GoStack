bc35f6598b186fd0f5b4846b2a0dbcf7ffd1dbe4
Gabriel Freitas <ogabrielfreitas@gmail.com>
2023-09-23 00:53:09 -0300

    Learning prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import routes from './routes';
 
+
 const app = express();
 
+app.use(express.json());
+// app.use(routes);
 
 app.get('/', (request, response) => response.json({message: 'Hello World'}));
 
---
+++ backend/z_trash/database/data-source.ts
@@ -0,0 +1,21 @@
+// import { DataSource } from "typeorm";
+
+// export const AppDataSource = new DataSource({
+//     type: "postgres",
+//     host: "localhost",
+//     port: 5432,
+//     username: "postgres",
+//     password: "docker",
+//     database: "gostack_postgres",
+//     synchronize: true,
+//     logging: true,
+//     entities: [
+//         "entity/*.js"
+//     ],
+//     subscribers: [
+//         "subscriber/*.js"
+//     ],
+//     migrations: [
+//         "migration/*.js"
+//     ]
+// })
